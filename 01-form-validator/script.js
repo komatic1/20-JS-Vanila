72f0b65f9ac9d1201f9a1f5962064613c7402ac3
Dima Dmitriev <komatic.dda@gmail.com>
2021-02-14 12:44:37 +0200

    Form validator

---
+++ 01-form-validator/script.js
@@ -0,0 +1,76 @@
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+
+// show input error message
+function showError (input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// show success
+function showSuccess (input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// check email
+function checkEmail (input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+// get field name
+function getFieldName (input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// check length
+function checkLength (input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// password confirm
+function checkPasswordMatch (input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Password do not match');
+    }
+}
+
+// check required fields
+function checkRequired (inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// form event listener
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
+});
